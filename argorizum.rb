725815f204b555d5c8570744f028aa9695f6dcc7
kurumakazuto <153343673+kurumakazuto@users.noreply.github.com>
2024-05-08 09:13:14 +0000

    [Fix]argorizum.rb アルゴリズムの練習

---
+++ argorizum.rb
@@ -0,0 +1,17 @@
+def multiple(num1,num2,num3)
+  count = []
+  (1..num1).each do |result|
+    if result % num2 == 0 or result % num3 == 0
+      count << result
+    end
+  end
+  puts count.length
+end
+
+puts "整数N,X,Yを入力してください"
+num1 = gets.to_i
+num2 = gets.to_i
+num3 = gets.to_i
+multiple(num1,num2,num3)
+
+#アルゴリズム2.4.4
